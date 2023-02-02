f5d97ca6e5013565dfdd65e40363477313788a78
jesalshah14 <jesalshah14@gmail.com>
2023-02-01 17:42:07 -0800

    commit code

--- Service1/service1.py
+++ Service1/service1.py
@@ -1,1 +1,23 @@
-sdcsd
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/zipcode', methods=['GET'])
+def get_zip_code():
+    
+    ZIP_CODES = {
+    "New York": "10001",
+    "Los Angeles": "90001",
+    "Chicago": "60601",
+    "Newark" : "94560"
+}
+
+    city = request.args.get('city')
+    if city is None:
+        return " Please provide a city name", 400
+    zip_code = f" Zip code for {city}  is {ZIP_CODES.get(city)}"
+    if zip_code is None:
+        return f" Zip code for {city} not found", 404
+    return zip_code
+
+if __name__ == '__main__':
+    app.run()
--- Service2/service2.py
+++ Service2/service2.py
@@ -1,1 +1,16 @@
-sdcds
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/weather", methods=["GET"])
+def weather():
+    
+    zip_code = request.args.get("zip")
+    api_key = "828e27fa703c4550bd42a66963dc073a"
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip_code}&appid={api_key}"
+    weather_data = requests.get(weather_url).json()
+    return weather_data
+
+if __name__ == "__main__":
+    app.run(debug=True)
