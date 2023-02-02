968c7a53a562010daa6414eb92970aec21246e83
jesalshah14 <jesalshah14@gmail.com>
2023-02-02 12:45:30 -0800

    hardcoded the code

--- Service2/service2.py
+++ Service2/service2.py
@@ -1,16 +1,31 @@
 from flask import Flask, request
-import requests
 
 app = Flask(__name__)
 
-@app.route("/weather", methods=["GET"])
+@app.route('/weather', methods=['GET'])
 def weather():
-    
-    zip_code = request.args.get("zip")
-    api_key = "828e27fa703c4550bd42a66963dc073a"
-    weather_url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip_code}&appid={api_key}"
-    weather_data = requests.get(weather_url).json()
-    return weather_data
+    zip_code = request.args.get('zip')
+    if zip_code:
+        weather_data = get_weather_data(zip_code)
+        return weather_data, 200
+    else:
+        return 'Error: No zip code provided', 400
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def get_weather_data(zip_code):
+    if zip_code == '94560':
+        weathearData = ' "weather": [{"description": "Clear Sky","temperature": "72","humidity": 30,"main": "Clear"}]'
+    elif zip_code == '90001':
+        weathearData = ' "weather": [{"description": "Rainy/FrezingRain","temperature": "80","humidity": 60,"main": "Rainy"}]'
+    elif zip_code == '10001':
+        weathearData = ' "weather": [{"description": "Snow Showers","temperature": "50","humidity": 50,"main": "Snow"}]'
+    elif zip_code == '60601':
+        weathearData = ' "weather": [{"description": "Partly Cloudy","temperature": "10","humidity": 10,"main": "Cloudy"}]'
+    elif zip_code == '94550':
+        weathearData = ' "weather": [{"description": "Mostly Sunny","temperature": "85","humidity": 20,"main": "Sunny"}]'
+    else:
+        weathearData ='No Information'
+    
+    return weathearData
+
+if __name__ == '__main__':
+    app.run()
